5c56d1bfd1e422e0c12c0ae901497c039b7f5cb7
dark0dave <dark0dave@mykolab.com>
2025-09-11 19:03:31 +0100

    fix(bugs): Fixing some bugs and inconsitencies
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- zdbae/dialog/zdbae25p.d
+++ zdbae/dialog/zdbae25p.d
@@ -1,13 +1,13 @@
 BEGIN ZDBAE25P
 
 IF ~Global("ZDBAE25KickedOut","GLOBAL",1)~ THEN BEGIN 0
-  SAY ~I hope you're satisfied. Look at my clothing! Caked-on mud everywhere! Do you have any idea how many hairy, four-legged BEASTS I had to avoid while making my way here? Woofs, were they? Wharves? I don't know.~ 
+  SAY ~I hope you're satisfied. Look at my clothing! Caked-on mud everywhere! Do you have any idea how many hairy, four-legged BEASTS I had to avoid while making my way here? Woofs, were they? Wharves? I don't know.~
   IF ~~ THEN REPLY ~Hello to you too, Baeloth. I have need of you. Will you join me?~ GOTO 1
   IF ~~ THEN REPLY ~Well, keep it up. You're doing a good job so far.~ GOTO 2
 END
 
 IF ~~ THEN BEGIN 1
-  SAY ~YES! At last, some protection! Protection for YOU, that is. I will join you. So that you can feel safe.~ 
+  SAY ~YES! At last, some protection! Protection for YOU, that is. I will join you. So that you can feel safe.~
   IF ~~ THEN DO ~SetGlobal("ZDBAE25KickedOut","GLOBAL",0) JoinParty()~ EXIT
 END
 
